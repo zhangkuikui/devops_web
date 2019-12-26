33c72a56ca637b7f584ff2a2c1a124f91c43c040
zhangkuikui <zhangkuikui@didapinche.com>
2019-12-26 16:33:09 +0800

    同步

--- devops_web/src/components/Xterm.js
+++ devops_web/src/components/Xterm.js
@@ -1,7 +1,14 @@
-import { Terminal } from 'xterm'
-import * as fit from 'xterm/lib/addons/fit/fit'
-import * as attach from 'xterm/lib/addons/attach/attach'
-Terminal.applyAddon(fit);
-Terminal.applyAddon(attach);
-
-export default Terminal
+// import { Terminal } from 'xterm'
+// import * as fit from 'xterm/lib/addons/fit/fit'
+// import * as attach from 'xterm/lib/addons/attach/attach'
+//
+//
+//
+// Terminal.applyAddon(fit);
+// Terminal.applyAddon(attach);
+//
+// import 'xterm/dist/addons/fullscreen/fullscreen.css'//如果不成功，请检查路径
+// import * as fullscreen from 'xterm/lib/addons/fullscreen/fullscreen'
+// Terminal.applyAddon(fullscreen);  // Apply the `fullscreen` addon
+//
+// export default Terminal
